2f12ad79c7067c1eb5cc20b3507b6862b832be73
cherif <tengacherif@gmail.com>
2026-03-11 19:09:34 +0000

    Ajout de la retrieval pipeline

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -39,35 +39,3 @@ print("---Context---")
 for i,doc in enumerate(relevants_docs,1):
     print(f"Document{i}:;\n {doc.page_content}\n")
     
-#Crée une application en faisant appel a un llm
-#Combiné la question et les documents
-
-combine_input = f"""En te basant sur les prochains documents , répond a cette question:{query}
-Documents:
-{chr(10).join([f"-{doc.page_content}" for doc in relevants_docs])}
-
-S'il te plait fournis une réponse claire et utile en te basant uniquement sur ces documents.Si tu ne peux pas trouveer la réponse dans les documents , dit"Je n'ai pas assez d'information pour repondre a la question basé sur le précédant document."
-
-"""
-
-#Create a Genai
-
-model = GoogleGenerativeAI(model="")
-
-#Definor le message pour le model
-messages = [
-    SystemMessage(content="Tu es un assistant très utile."),
-    HumanMessage(content=combine_input)
-]
-
-#Appeler le resultat
-
-result = model.invoke(messages)
-
-#Afficher la reponse
-
-print(f"\n---Reponse Generer!!---")
-#Full result
-#print(result)
-print("Content Only:")
-print(result.content)
